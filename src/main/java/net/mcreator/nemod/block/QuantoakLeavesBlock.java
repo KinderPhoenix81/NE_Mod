ea00cfbc79e43930996a252195a43a14e487a216
AwesomeEthan02 <21ethabeau@20-H207-19.denmark.k12.wi.us>
2021-04-07 15:10:39 -0500

    Ethan

---
+++ src/main/java/net/mcreator/nemod/block/QuantoakLeavesBlock.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.nemod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.nemod.NeModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@NeModModElements.ModElement.Tag
+public class QuantoakLeavesBlock extends NeModModElements.ModElement {
+	@ObjectHolder("ne_mod:quantoak_leaves")
+	public static final Block block = null;
+	public QuantoakLeavesBlock(NeModModElements instance) {
+		super(instance, 4);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends LeavesBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.LEAVES).sound(SoundType.PLANT).hardnessAndResistance(1f, 1f).lightValue(0).notSolid());
+			setRegistryName("quantoak_leaves");
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 150;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
