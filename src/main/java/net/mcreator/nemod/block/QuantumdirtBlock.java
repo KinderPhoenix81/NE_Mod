18c02c8c85ac63f483b8357ca754cb103d79d963
AwesomeEthan02 <21ethabeau@20-H207-19.denmark.k12.wi.us>
2021-04-08 14:15:56 -0500

    eee

---
+++ src/main/java/net/mcreator/nemod/block/QuantumdirtBlock.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.nemod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.nemod.NeModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@NeModModElements.ModElement.Tag
+public class QuantumdirtBlock extends NeModModElements.ModElement {
+	@ObjectHolder("ne_mod:quantumdirt")
+	public static final Block block = null;
+	public QuantumdirtBlock(NeModModElements instance) {
+		super(instance, 17);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("quantumdirt");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
