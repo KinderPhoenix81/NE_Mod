dfb9196dff6e310eafe9ac0bbb55a7eda2cd6c2b
KinderPhoenix81 <23alexalle@20-H207-18.denmark.k12.wi.us>
2021-04-09 14:34:30 -0500

    finished functionality with warped tools

--- src/main/java/net/mcreator/nemod/item/Warped_BrimstoneShovelItem.java
+++ src/main/java/net/mcreator/nemod/item/Warped_BrimstoneShovelItem.java
@@ -3,14 +3,23 @@ package net.mcreator.nemod.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.ShovelItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
 
+import net.mcreator.nemod.procedures.WarpedBrimstoneShovelLivingEntityIsHitWithToolProcedure;
+import net.mcreator.nemod.procedures.WarpedBrimstoneShovelBlockDestroyedWithToolProcedure;
 import net.mcreator.nemod.NeModModElements;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @NeModModElements.ModElement.Tag
 public class Warped_BrimstoneShovelItem extends NeModModElements.ModElement {
@@ -24,29 +33,60 @@ public class Warped_BrimstoneShovelItem extends NeModModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ShovelItem(new IItemTier() {
 			public int getMaxUses() {
-				return 250;
+				return 905;
 			}
 
 			public float getEfficiency() {
-				return 6f;
+				return 7f;
 			}
 
 			public float getAttackDamage() {
-				return 0f;
+				return 2f;
 			}
 
 			public int getHarvestLevel() {
-				return 2;
+				return 3;
 			}
 
 			public int getEnchantability() {
-				return 14;
+				return 10;
 			}
 
 			public Ingredient getRepairMaterial() {
 				return Ingredient.fromStacks(new ItemStack(Warped_BrimstoneIngotItem.block, (int) (1)));
 			}
 		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+			@Override
+			public boolean onBlockDestroyed(ItemStack itemstack, World world, BlockState bl, BlockPos pos, LivingEntity entity) {
+				boolean retval = super.onBlockDestroyed(itemstack, world, bl, pos, entity);
+				int x = pos.getX();
+				int y = pos.getY();
+				int z = pos.getZ();
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("x", x);
+					$_dependencies.put("y", y);
+					$_dependencies.put("z", z);
+					$_dependencies.put("world", world);
+					WarpedBrimstoneShovelBlockDestroyedWithToolProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
+
+			@Override
+			public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
+				boolean retval = super.hitEntity(itemstack, entity, sourceentity);
+				double x = entity.getPosX();
+				double y = entity.getPosY();
+				double z = entity.getPosZ();
+				World world = entity.world;
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("entity", entity);
+					WarpedBrimstoneShovelLivingEntityIsHitWithToolProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
 		}.setRegistryName("warped_brimstone_shovel"));
 	}
 }
